1257c2be19f73d0f1d6225ad4efb11f4bb9c94da
Christopher Patti <feoh@feoh.org>
2025-08-14 18:41:03 -0400

    feat: Switch gears and calculate readability score

--- how-readable-are-pybites-articles.py
+++ how-readable-are-pybites-articles.py
@@ -2,11 +2,12 @@ import lxml  # noqa: F401
 from bs4 import BeautifulSoup
 from httpx import get
 from os import makedirs
+from textstat import flesch_kincaid_grade
 
 def main():
     site_map_resp = get("https://pybit.es/post-sitemap1.xml")
-    soup = BeautifulSoup(site_map_resp.content, 'xml')
-    articles = [ loc_tag.get_text() for loc_tag in soup.find_all('loc') ]
+    xsoup = BeautifulSoup(site_map_resp.content, 'xml')
+    articles = [ loc_tag.get_text() for loc_tag in xsoup.find_all('loc') ]
     makedirs("pybites-articles", exist_ok=True)
 
     for article in articles[1:]:
@@ -14,10 +15,10 @@ def main():
         if"https://pybit.es/wp-content/uploads" in article:
            continue 
         article_resp = get(article)
-        print(f"Downloading article: {title_url_chunk}")
+        hsoup = BeautifulSoup(article_resp.content, 'html.parser')
         article_filename = f"pybites-articles/{title_url_chunk}"
-        with open(article_filename, "w") as fp:
-            fp.write(article_resp.text)
+        rounded_flesch_kincaid_grade_level = round(flesch_kincaid_grade("".join(hsoup.stripped_strings)))
+        print(f"According to the Flesch/Kincaid Grade, Pybites article {article_filename} is readable by a {rounded_flesch_kincaid_grade_level} grader.")
 
 
 
